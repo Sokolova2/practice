f55d0d094b4c777011cec8a78e35547dd52346e2
Karina <karinasokolova252@gmail.com>
2025-06-02 20:58:49 +0300

    Remove unnecessary import for using subjects from file .env. Fixed method from creat

--- src/main.py
+++ src/main.py
@@ -1,20 +1,18 @@
-import os
 from fastapi import FastAPI, Depends
-from dotenv import load_dotenv
 import uvicorn
 from src.database.database import engine, Base
-
-load_dotenv()
+from src.database.fixtures.products_fixtures import create_product
 
 app = FastAPI()
 
 async def init_models():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.creat_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 @app.on_event("startup")
 async def on_startup():
     await init_models()
+    await create_product()
 
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
