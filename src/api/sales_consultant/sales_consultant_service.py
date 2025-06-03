7224cc5161c66961fc507c743e3ebece14c19cd4
Karina <karinasokolova252@gmail.com>
2025-06-03 19:54:53 +0300

    Added method for getting last order and for change status order

---
+++ src/api/sales_consultant/sales_consultant_service.py
@@ -0,0 +1,23 @@
+from fastapi import HTTPException
+from sqlalchemy import select, desc
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.database.models.orders.orders import OrdersModel
+from src.database.schemas.orders.orders import OrdersUpdateSchemas
+
+class SalesConsultantService:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_order(self):
+        result = await self.db.execute(
+            select(OrdersModel).order_by(desc(OrdersModel.id_order)).limit(1))
+        orders = result.scalars().first()
+        return orders
+    
+    async def change_status(self, id_order: int, order: OrdersUpdateSchemas):
+        stmt = select(OrdersModel).where(OrdersModel.id_order == id_order)
+        result = await self.db.execute(stmt)
+        order = result.scalars().first()
+
+        if order == None: 
+            raise HTTPException(status_code=404, detail="Order not found")
