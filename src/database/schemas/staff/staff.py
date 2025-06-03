161720267616d3cf61aa4daf2075acb3543f58f5
Karina <karinasokolova252@gmail.com>
2025-06-03 11:14:12 +0300

    In StaffAddSchemas remove id and fixed last_name from (max_length=1, max_length=70) to (min_length=1, max_length=70). Add StaffGetSchemas for getting staff. Add StaffLoginSchemas for authorization for access

--- src/database/schemas/staff/staff.py
+++ src/database/schemas/staff/staff.py
@@ -1,10 +1,23 @@
 from pydantic import BaseModel, Field
 
 class StaffAddSchemas(BaseModel):
-
-    id: int 
-    last_name: str = Field(max_length=1, max_length=70)
+ 
+    last_name: str = Field(min_length=1, max_length=70)
     first_name: str = Field(min_length=1, max_length=70)
     login: str = Field(min_length=1, max_length=70)
     password: str = Field(min_length=8, max_length=128)
     role: str 
+
+class StaffGetSchemas(BaseModel):
+
+    id: int
+    last_name: str
+    first_name: str
+    login: str
+    password: str
+    role: str
+
+class StaffLoginSchemas(BaseModel):
+
+    login: str
+    password: str
