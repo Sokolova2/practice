5760809d432e241ca1464cd9c83c45b8b0de5e36
Karina <karinasokolova252@gmail.com>
2025-06-03 12:03:19 +0300

    Add route for cashier_routes from cashier_controller.py

--- src/main.py
+++ src/main.py
@@ -4,10 +4,12 @@ from src.database.database import engine, Base
 from src.database.fixtures.products_fixtures import create_product
 from src.database.models.staff.staff import StaffModels
 from src.api.auth.auth_controller import auth_routes
+from src.api.cashier.cashier_controller import cashier_routes
 
 app = FastAPI()
 
 app.include_router(auth_routes, tags=["Auth servise"])
+app.include_router(cashier_routes, tags=["Cashier service"])
 
 async def init_models():
     async with engine.begin() as conn:
