56cef417536bb5e399646dcce3067b1e3626fc5a
Karina <karinasokolova252@gmail.com>
2025-06-03 11:16:15 +0300

    Add auth for add user and login for getting access

---
+++ src/api/auth/auth_controller.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends, Response
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.database.database import get_session
+from http import HTTPStatus
+from src.api.auth.auth_service import AuthService
+from src.database.schemas.staff.staff import StaffAddSchemas, StaffLoginSchemas, StaffGetSchemas
+auth_routes = APIRouter(
+    prefix="/auth"
+)
+
+@auth_routes.post("/new", summary="Add user")
+async def add_user(user: StaffAddSchemas, db: AsyncSession = Depends(get_session)):
+    service = AuthService(db)
+    return await service.add_user(users=user)
+
+@auth_routes.post("/login", summary="Login user")
+async def login(creds: StaffLoginSchemas, response: Response, db: AsyncSession = Depends(get_session)):
+    service = AuthService(db)
+    return await service.login(creds=creds, responce=response)
+
+@auth_routes.get("/staff", summary="Get staff")
+async def get_user(db: AsyncSession = Depends(get_session)):
+    service = AuthService(db)
+    return await service.get_user()
---
+++ src/api/auth/auth_service.py
@@ -0,0 +1,101 @@
+import os
+from fastapi import HTTPException, Response
+from dotenv import load_dotenv
+from authx import AuthX, AuthXConfig
+from passlib.context import CryptContext
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.database.models.staff.staff import StaffModels
+from src.database.schemas.staff.staff import StaffAddSchemas, StaffLoginSchemas, StaffGetSchemas
+from datetime import datetime
+import jwt
+from typing import List
+
+load_dotenv()
+
+config = AuthXConfig(
+    JWT_SECRET_KEY=os.getenv("JWT_SECRET"),
+    JWT_ACCESS_COOKIE_NAME=os.getenv("JWT_ACCESS_COOKIE_NAME"),
+    JWT_TOKEN_LOCATION=["cookies"]
+)
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+authx = AuthX(config=config)
+
+class AuthService:
+    def __init__(self, db:AsyncSession):
+        self.db = db
+
+    async def add_user(self, users: StaffAddSchemas):
+        stmt = select(StaffModels).where(StaffModels.login == users.login)
+        result = await self.db.execute(stmt)
+        existing_user = result.scalars().first()
+
+        if existing_user:
+            raise HTTPException(status_code=400, detail="User with this login already exists ")
+        
+        add_user = StaffModels(
+            last_name = users.last_name,
+            first_name = users.first_name,
+            login = users.login,
+            password = hash_password(users.password),
+            role = users.role
+        )
+        self.db.add(add_user)
+        await self.db.commit()
+        await self.db.refresh(add_user)
+
+        return{
+            "message": f"Successfully add user",
+            "NewUser": add_user
+        }
+    
+    def create_jwt_token(self, id_user: str, login: str, role: str, secret_key:str):
+        now = datetime.now()
+        payload = {
+            "sub": id_user,
+            "login": login,
+            "role": role,
+            "iat": int(now.timestamp()),
+            "type": "access"
+        }
+        token = jwt.encode(payload, secret_key, algorithm="HS256")
+        return token
+
+    async def login(self, creds: StaffLoginSchemas, response: Response):
+        stmt = select(StaffModels).where(StaffModels.login == creds.login)
+        result = await self.db.execute(stmt)
+        user = result.scalar_one_or_none()
+
+        if not user:
+            raise HTTPException(status_code=401, detail="User not found")
+        
+        if not pwd_context.verify(creds.password, user.password):
+            raise HTTPException(status_code=403, detail="Invalid password")
+        
+        token = self.create_jwt_token(
+            id_user = str(user.id),
+            login = user.login,
+            role = user.role,
+            secret_key=os.getenv("JWT_SECRET")
+        )
+
+        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+
+        return{
+            "message": f"Successfully login: {user.last_name + user.first_name}",
+            "access_token": token,
+            "token-type": "bearer",
+            "id_user": user.id,
+            "login": user.login,
+            "role": user.role
+        }
+    
+    async def get_user(self) -> List[StaffGetSchemas]:
+            result = await self.db.execute(select(StaffModels))
+            get_user = result.scalars().all()
+            return get_user
