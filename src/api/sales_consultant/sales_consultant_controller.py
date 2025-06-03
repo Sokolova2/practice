febe3f1985e2bfabaa437f3c0b3dc179cf237fa6
Karina <karinasokolova252@gmail.com>
2025-06-03 20:16:19 +0300

    Add route for method chamge_status from sales_consultant_service.py

--- src/api/sales_consultant/sales_consultant_controller.py
+++ src/api/sales_consultant/sales_consultant_controller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.database import get_session
 from src.api.sales_consultant.sales_consultant_service import SalesConsultantService
+from src.database.schemas.orders.orders import OrdersUpdateSchemas
 
 sales_consultant_routes = APIRouter(
     prefix="/consultant"
@@ -11,3 +12,12 @@ sales_consultant_routes = APIRouter(
 async def get_order(db: AsyncSession = Depends(get_session)):
     service = SalesConsultantService(db)
     return await service.get_order()
+
+@sales_consultant_routes.patch("/order/{id_order}", summary="Change status")
+async def change_status(
+    id_order: int, 
+    new_order: OrdersUpdateSchemas, 
+    db:AsyncSession = Depends(get_session)
+):
+    service = SalesConsultantService(db)
+    return await service.change_status(id_order, new_order)
