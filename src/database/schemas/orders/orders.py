e0fc616dfdaf9cfcbc04606d43395ca31cd2dbdb
Karina <karinasokolova252@gmail.com>
2025-06-03 19:59:23 +0300

    Remove attribute discount from OrdersAddSchemas and added OrdersUpdateSchemas

--- src/database/schemas/orders/orders.py
+++ src/database/schemas/orders/orders.py
@@ -5,9 +5,12 @@ class OrdersGetSchemas(BaseModel):
     id_order: int
     name_product: str 
     status: str
+    price: float
     discount: float
     create_data: datetime
 
 class OrdersAddSchemas(BaseModel):
     id_product: int
-    discount: float
+
+class OrdersUpdateSchemas(BaseModel):
+    status: str
