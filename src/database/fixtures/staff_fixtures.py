7d56a567933786e3ad529e212cee74557bd492fc
Karina <karinasokolova252@gmail.com>
2025-06-03 22:10:08 +0300

    Fixed data

--- src/database/fixtures/staff_fixtures.py
+++ src/database/fixtures/staff_fixtures.py
@@ -9,9 +9,9 @@ async def create_user():
 
         if not product:
             users = [
-                StaffModels(last_name="Петров", fist_name="Петро", login="petrov_petro", password="12345678", role="Бухглатер"),
-                StaffModels(last_name="Іваненко", fist_name="Іван", login="ivanenko_ivan", password="12345678", role="Продавец-консультант"),
-                StaffModels(last_name="Коваленко", fist_name="Марина", login="kovalenko_marina", password="12345678", role="Касир"),
+                StaffModels(last_name="Петров", first_name="Петро", login="petrov_petro", password="12345678", role="Бухглатер"),
+                StaffModels(last_name="Іваненко", first_name="Іван", login="ivanenko_ivan", password="12345678", role="Продавец-консультант"),
+                StaffModels(last_name="Коваленко", first_name="Марина", login="kovalenko_marina", password="12345678", role="Касир"),
             ]
             session.add_all(users)
             await session.commit()
