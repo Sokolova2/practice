a1be3084596fc42c44274f3eca64233fc7de68cb
Karina <karinasokolova252@gmail.com>
2025-06-03 22:09:26 +0300

    Fixed data for product added create_data

--- src/database/fixtures/products_fixtures.py
+++ src/database/fixtures/products_fixtures.py
@@ -1,6 +1,7 @@
 from src.database.models.products.products import ProductsModel
 from src.database.database import new_async_session
 from sqlalchemy import select
+from datetime import datetime
 
 async def create_product():
     async with new_async_session() as session:
@@ -9,13 +10,13 @@ async def create_product():
 
         if not product:
             products = [
-                ProductsModel(name="Ноутбук Lenovo", price=21000.0),
-                ProductsModel(name="Телефон Samsung", price=11000.0),
-                ProductsModel(name="Миша Logitech", price=500.0),
-                ProductsModel(name="Комп`ютер Intell", price=33000.0),
-                ProductsModel(name="Клавіатура Hator", price=500.0),
-                ProductsModel(name="Телефон Iphone 13", price=25000.0),
-                ProductsModel(name="Подовжувач Gelius", price=1000.0),
+                ProductsModel(name="Ноутбук Lenovo", price=21000.0, create_data=datetime(2025, 4, 25)),
+                ProductsModel(name="Телефон Samsung", price=11000.0, create_data=datetime(2025, 5, 5)),
+                ProductsModel(name="Миша Logitech", price=500.0, create_data=datetime(2025, 4, 1)),
+                ProductsModel(name="Комп`ютер Intell", price=33000.0, create_data=datetime(2025, 5, 25)),
+                ProductsModel(name="Клавіатура Hator", price=500.0, create_data=datetime(2025, 4, 29)),
+                ProductsModel(name="Телефон Iphone 13", price=25000.0, create_data=datetime(2025, 5, 27)),
+                ProductsModel(name="Подовжувач Gelius", price=1000.0, create_data=datetime(2025, 4, 19)),
             ]
             session.add_all(products)
             await session.commit()
